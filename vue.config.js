852a2ab9d8b88805a3411dbd6ee34f504a24385f
Carlos Lazaro <carlos.e.lazaro@hotmail.com>
2024-09-10 20:18:08 -0300

    Modificaciones Manifest

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pwa: {
+    name: 'Carlito',
+    shortName: 'VerCar',
+    themeColor: '#0925f1 ',
+    msTileColor: '#ff0101',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    startUrl: ".",
+    display: "standalone",
+    backgroundColor: "#000000"
+  }
 })
