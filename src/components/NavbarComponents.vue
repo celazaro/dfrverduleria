6d1e97ea93634e3c6b5cf2165728867f72beae4b
Carlos Lazaro <carlos.e.lazaro@hotmail.com>
2024-09-10 21:20:43 -0300

    Modificaciones varias 1

--- src/components/NavbarComponents.vue
+++ src/components/NavbarComponents.vue
@@ -3,13 +3,13 @@
 <!-- Navbar start -->
 <div class="container px-0">
     <nav class="navbar navbar-light bg-white navbar-expand-xl">
-        <a href="index.html" class="navbar-brand"><h1 class="text-primary display-6">Verdulería</h1></a>
+        <a href="index.html" class="navbar-brand"><h1 class="text-primary display-6">VyF Carlitos</h1></a>
         <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="fa fa-bars text-primary"></span>
         </button>
         <div class="collapse navbar-collapse bg-white" id="navbarCollapse">
             <div class="navbar-nav mx-auto">
-            <router-link to="/" class="nav-item nav-link">Home</router-link> 
+            <router-link to="/" class="nav-item nav-link">Inicio</router-link> 
             <router-link to="/about" class="nav-item nav-link">Contacto</router-link>
                 
             </div>
--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,14 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   pwa: {
-    name: 'Carlito',
+    name: 'Verduleria',
     shortName: 'VerCar',
     themeColor: '#0925f1 ',
     msTileColor: '#ff0101',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
 
-    startUrl: ".",
+    startUrl: "./about",
     display: "standalone",
     backgroundColor: "#000000"
   }
